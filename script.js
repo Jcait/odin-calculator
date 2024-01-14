bf81e5f39aaff2b5d1f4135a5b3f68da7327f487
Jo <134626490+Jcait@users.noreply.github.com>
2024-01-14 11:40:55 +0000

    add math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+let add = (num1, num2) =>  {
+    return num1 + num2
+}
+
+let subtract = (num1, num2) =>  {
+    return num1 - num2
+}
+
+let multiply = (num1, num2) =>  {
+    return num1 * num2
+}
+
+let divide = (num1, num2) =>  {
+    return num1 / num2
+}
