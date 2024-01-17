448adb6579e3453c4121a1e91132bd5a74415f9b
Jo <134626490+Jcait@users.noreply.github.com>
2024-01-17 13:58:02 +0000

    code refactor

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 let btnNum = document.querySelectorAll('.calc-number')
 let btnOperator = document.querySelectorAll('.calc-operator')
 let display = document.querySelector('input')
-// let point = document.querySelector('.point')
-// let btnDel = document.querySelector('.delete-calc')
-// let btnClear =  document.querySelector('.clear-calc')
 let btnSum = document.querySelector('.calc-equals')
-// let btn = document.querySelectorAll('button')
-// let currentNum = ""
-// let btnText = ""
 let currentNum = []
 let storedNum = []
 let  operator 
@@ -30,8 +24,23 @@ let divide = (num1, num2) => {
     return num1 / num2  
 }
 let updateDisplay = (button) => {
-    currentNum.push(button.innerText)
+    currentNum.push(Number(button.innerText))
     display.value = display.value + button.innerText
+}
+
+let calculate = () => {
+    let firstNum = Number(storedNum);
+    let secondNum = Number(currentNum.join("").replace(/\D/g,''))
+    console.log(currentNum)
+    console.log(firstNum)
+    console.log(storedNum)
+    console.log(secondNum)
+    console.log(operator(firstNum, secondNum))
+    currentNum = []
+    storedNum.push(operator(firstNum, secondNum))
+    display.value = operator(firstNum, secondNum)
+    storedNum.shift()
+    operator = ""
 }
 
 // NUMBER BUTTONS
@@ -39,7 +48,8 @@ let updateDisplay = (button) => {
 btnNum.forEach(button => {
     button.addEventListener('click', () => {
         if(operator && !storedNum.length) {
-            storedNum = currentNum.concat()
+            let joinedArr = (currentNum.join(""))
+            storedNum.push(Number(joinedArr))
             currentNum = []
             updateDisplay(button)
         } else if(!operator && storedNum.length) {
@@ -57,16 +67,25 @@ btnNum.forEach(button => {
 
 btnOperator.forEach(button => {
     button.addEventListener('click', () => {
-        if(!operator){
-            updateDisplay(button)
-        } else if(currentNum.includes(button.innerText)
-        || !currentNum.length) {
-            return ""
-        } else if(!currentNum.includes(button.innerText)){
-            console.log("test")
-            currentNum.pop()
-            currentNum.push(button.innerText)
-            display.value = currentNum.join("")
+        let i = display.value.length
+        let dupeSymbol = display.value.charAt(i-1)
+            if(operator
+            && currentNum.length
+            && storedNum.length) {
+                calculate()
+                display.value = display.value + `${button.innerText}`
+        }
+        else if(!operator){
+            display.value = display.value + `${button.innerText}`
+        } 
+        else if(!storedNum.length) {
+            display.value = display.value.replace(dupeSymbol, button.innerText)
+
+            console.log("replaced")
+        }
+        else if (!currentNum.length && storedNum.length){
+            display.value = display.value.replace(dupeSymbol, button.innerText)
+
         }
     })
 })
@@ -105,168 +124,13 @@ btnOperator.forEach(button => {
 // Equals button, own func for readability
 
 btnSum.addEventListener('click', () => {
-    let firstNum = Number(storedNum.join("").replace(/\D/g,''));
-    let secondNum = Number(currentNum.join("").replace(/\D/g,''))
-    // console.log(currentNum)
-    // console.log(firstNum)
-    // console.log(storedNum)
-    // console.log(secondNum)
-    console.log(operator(firstNum, secondNum))
-    currentNum = []
-    storedNum = operator(firstNum, secondNum).toString().split("")
-    display.value = storedNum.join("")
-    operator = ""
+    calculate()
     
 })
 
 
 
-// let operate = (storedNum, operator, currentNum) => {
-//         return operator(currentNum, storedNum)
-//     }
-
-//     let calculate = {
-//         sum() { 
-//             return operate(this.current, this.operator, this.store)
-//         }   
-//     }
-    
 
 
-// let updateDisplay = (btn) =>  {
-//     currentNum = currentNum + btn.innerText.toString()
-//     display.value = display.value + `${btn.innerText}`
-// }
-
-
-// btnNum.forEach(button => {
-//     button.addEventListener('click', () => {
-//         updateDisplay(button)
-//         calculate.current = Number(currentNum)
-//         if(calculate.summed) {
-//             display.value = currentNum
-//             calculate.summed = false
-//         }
-//     }) 
-// })
-
-
-// point.addEventListener('click', () => {
-//     if (currentNum.includes(".") || currentNum == "") {
-//         return ""
-//     } else {
-//         updateDisplay(point)
-//     }
-// })
-
-
-
-// btnDel.addEventListener('click', () => {
-//     currentNum = ""
-//     display.value = ""
-//     btnText = ""
-//     calculate = {
-//         sum() {
-//             return operate(this.current, this.operator, this.store)
-//         }
-//     }
-// })
-// btnClear.addEventListener('click', () => {
-//     if(!calculate.store) {
-//         console.log("wew")
-//         display.value = ""
-//         return ""
-//     } else {
-//         display.value = `${calculate.store}`
-//     }
-// })
-// btnClear.addEventListener('click', () => {
-//     delete calculate.current
-//     delete calculate.operator
-//     currentNum = ""
-//     btnText = ""
-// })
-
-
-
-// btnOperator.forEach(button => {
-//     button.addEventListener('click', () => { {
-//             if(!calculate.store 
-//                 && calculate.current ) {
-//             // console.log("click")
-//             calculate.store = Number(calculate.current)
-//             delete calculate.current
-//         } 
-//         else if (calculate.current === 0) {
-//             display.value = calculate.sum()
-//             calculate.store === 0
-//             display.value  === ""
-
-//         }
-        
-//         else if(calculate.current 
-//             && (calculate.store || calculate.store === 0)) {
-//             calculate.store = calculate.sum()
-//             display.value = `${calculate.store} ${btnText} `
-//             delete calculate.current
-//         }    
-//     } 
-
-//             currentNum = ""  
-
-//     })
-// })
-
-// btnOperator.forEach(button => {
-//     button.addEventListener('click', () => {
-//                 if(calculate.current === 0) {
-//             display.value = display.value + ` ${button.innerText} `
-//             btnText = ""   
-//         }
-//         else if(btnText === button.innerText ) {
-//             console.log("woof")
-//             return ""            
-//         } 
-//         else if (btnText && button.innerText !== btnText) {
-//             console.log(btnText)
-//             console.log(button.innerText)
-//             console.log("meow")
-//             display.value = display.value.replace(btnText, button.innerText)
-//         } 
-
-//         else {
-//             display.value = display.value + ` ${button.innerText} `
-//             btnText = ""
-//         }
-//     })
-// })
-
-
-
-
-
-
-
-
-
-// btnSum.addEventListener('click', () => {
-//     console.log("click!")
-//     calculate.summed = true
-  
-//     if(!calculate.store && calculate!== 0) {
-//         return ""
-//     }
-//     else if(calculate.store === 0) {
-//         display.value = display.value + ` ${button.innerText} `
-//     }
-//     calculate.current = calculate.sum()
-//     display.value = Number(calculate.current)
-//     currentNum = ""
-//     delete calculate.store
-//     if(!currentNum && calculate.current){
-//         return ""
-//     }  
-
-// })
 
 
